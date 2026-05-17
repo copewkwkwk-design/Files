3d78654e11a1b28757c4e2bed6ad03e1880456c5
copewkwkwk-design <copewkwkwk@gmail.com>
2026-05-17 12:37:33 +0800

    Update botted.py

--- botted.py
+++ botted.py
@@ -15,12 +15,12 @@ class FloodButton(ui.View):
         super().__init__(timeout=None)
         self.msg = message_content
 
-    @ui.button(label="press2fludz (10x)", style=discord.ButtonStyle.danger)
+    @ui.button(label="press2fludz (5x)", style=discord.ButtonStyle.danger)
     async def flood_button(self, interaction: discord.Interaction, button: ui.Button):
         # Tell Discord we received the click immediately to prevent timeout
         await interaction.response.defer(ephemeral=True)
         
-        for _ in range(10):
+        for _ in range(5):
             try:
                 # Use followup for global external sending
                 await interaction.followup.send(self.msg)
@@ -45,7 +45,8 @@ class MyBot(discord.Client):
             1394753272492851322,
             586843272924626944,
             1373933519356563497,
-            1135546221533077654
+            1135546221533077654,
+            1492300393814950018
         } 
 
     async def setup_hook(self):
